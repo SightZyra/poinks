f59f513b1a345a58ea54897931097bbe321b2b90
SarahHasler <sarahhasler@bluewin.ch>
2019-01-05 11:20:17 +0100

    feat(): new dice pur css

--- src/app/components/dice/dice.component.ts
+++ src/app/components/dice/dice.component.ts
@@ -1,28 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-dice',
   templateUrl: './dice.component.html',
   styleUrls: ['./dice.component.scss']
 })
-export class DiceComponent implements OnInit {
+export class DiceComponent implements OnInit, AfterViewInit {
   @Input() upsidedown = false;
+  private dice;
 
-  public diceNumber = 6;
-  public rotateDice = false;
-
-  constructor() {
+  constructor(private elementRef: ElementRef) {
   }
 
   ngOnInit() {
   }
 
-  public toggleDice() {
-    this.rotateDice = !this.rotateDice;
-    if (!this.rotateDice) {
-      this.diceNumber = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-      console.log('test', this.diceNumber);
-    }
+  ngAfterViewInit() {
+    this.dice = this.elementRef.nativeElement.querySelector('.dice');
   }
 
+  public rotateDice() {
+    const min = 1;
+    const max = 24;
+    const xRand = this.getRandom(max, min);
+    const yRand = this.getRandom(max, min);
+
+    this.dice.style.webkitTransform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
+    this.dice.style.transform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
+  }
+
+  private getRandom(max, min) {
+    return (Math.floor(Math.random() * (max - min)) + min) * 90;
+  }
 }
