334d466d9744b35108dd60b39ab7f81aeb9d836b
SarahHasler <sarahhasler@bluewin.ch>
2019-01-24 17:32:53 +0100

    feat: show dice for each player

--- src/app/components/who-will-start/who-will-start.component.ts
+++ src/app/components/who-will-start/who-will-start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PointsService} from '../points/points.service';
 
 @Component({
   selector: 'app-who-will-start',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./who-will-start.component.scss']
 })
 export class WhoWillStartComponent implements OnInit {
+  public playerCountAsArray: number[] = [];
+  public playerCount: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private pointsService: PointsService) {
   }
 
+  ngOnInit() {
+    this.pointsService.changePlayerCountSubject
+      .subscribe(count => {
+        this.playerCount = count;
+        this.createPlayerArray();
+      });
+  }
+
+  private createPlayerArray() {
+    this.playerCountAsArray = Array(this.playerCount).fill(1).map((x, i) => i + 1);
+  }
 }
